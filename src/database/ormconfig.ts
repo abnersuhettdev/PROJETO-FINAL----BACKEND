3542278313eb56ff448e3a2c335b086a013e9f50
Abner Suhett <suhettabner@gmail.com>
2023-08-29 20:21:59 -0300

    rimraf

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -1,5 +1,10 @@
 import "dotenv/config";
+
 import { DataSourceOptions } from "typeorm";
+import { NotesEntity } from "./entities/notes.entity";
+import { UsersEntity } from "./entities/users.entity";
+import { UsersMigration1692924597885 } from "./migrations/1692924597885-UsersMigration";
+import { NotesMigration1692924656170 } from "./migrations/1692924656170-NotesMigration";
 
 const env = process.env.NODE_ENV;
 
@@ -8,8 +13,8 @@ const configProd: DataSourceOptions = {
 	url: process.env.DATABASE_URL,
 	logging: false,
 	ssl: true,
-	migrations: ["./migrations/*.js"],
-	entities: ["./entities/*.entity.js"],
+	migrations: [UsersMigration1692924597885, NotesMigration1692924656170],
+	entities: [UsersEntity, NotesEntity],
 };
 
 const configDev: DataSourceOptions = {
@@ -19,8 +24,8 @@ const configDev: DataSourceOptions = {
 	ssl: {
 		rejectUnauthorized: false,
 	},
-	migrations: ["./migrations/*.{js,ts}"],
-	entities: ["./entities/*.entity.{js,ts}"],
+	migrations: [UsersMigration1692924597885, NotesMigration1692924656170],
+	entities: [UsersEntity, NotesEntity],
 };
 
 export default env === "production" ? configProd : configDev;
